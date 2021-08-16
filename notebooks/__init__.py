da1ae2ef99dfd294d7972e0b76ca3524a1a2a54c
Oscar Jacobsen <67653883+ocrj@users.noreply.github.com>
2021-08-16 02:28:34 +0200

    add

---
+++ notebooks/__init__.py
@@ -0,0 +1,10 @@
+# Add parent directory 
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+import src
